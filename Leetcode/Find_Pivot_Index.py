e2afb7fc7ae516d567e281213ca325ea5d2a935a
Ryan Gang <ryan-gg@outlook.com>
2022-09-18 15:20:02 +0530

    724. Find_Pivot_Index. Revision 1.

--- Leetcode/Find_Pivot_Index.py
+++ Leetcode/Find_Pivot_Index.py
@@ -2,9 +2,11 @@ from typing import List
 
 
 class Solution:
+    """
+    First attempt, ignore this solution."""
     # Runtime: 296 ms, faster than 24.64% of Python3 online submissions.
     # Memory Usage: 15.3 MB, less than 11.07% of Python3 online submissions.
-    def pivotIndex(self, nums: List[int]) -> int:
+    def pivotIndexChaos(self, nums: List[int]) -> int:
         n = len(nums)
         left = nums[:]
         right = nums[:]
@@ -41,20 +43,37 @@ class Solution:
     # My solution is needlessly chaotic, this is much better and clean.
     def pivotIndexClean(self, nums):
         S = sum(nums)
-        leftsum = 0
+        left_sum = 0
         for i, x in enumerate(nums):
-            if leftsum == (S - leftsum - x):
+            if left_sum == (S - left_sum - x):
                 return i
-            leftsum += x
+            left_sum += x
+        return -1
+
+    """
+    Initially prefix_sum = 0 and suffix_sum = sum(nums).
+    For every index in nums, first remove that from suffix_sum. (At this point none of prefix_sum
+    or suffix_sum contain the ith index.) Now compare if the sums are equal.
+    Then after the comparison add the ith index to the prefix_sum for the next iteration."""
+    # Runtime: 157 ms, faster than 93.91% of Python3 online submissions.
+    # Memory Usage: 15.3 MB, less than 11.31% of Python3 online submissions.
+    def pivotIndex(self, nums: List[int]) -> int:
+        suffix_sum = sum(nums)
+        prefix_sum = 0
+        for i, v in enumerate(nums):
+            suffix_sum -= v
+            if prefix_sum == suffix_sum:
+                return i
+            prefix_sum += v
         return -1
 
 
 sol = Solution()
-assert sol.pivotIndexClean(nums=[1, 7, 3, 6, 5, 6]) == 3
-assert sol.pivotIndexClean(nums=[1, 2, 3]) == -1
-assert sol.pivotIndexClean(nums=[1, 2, 1]) == 1
-assert sol.pivotIndexClean(nums=[2, 1, -1]) == 0
-assert sol.pivotIndexClean(nums=[1]) == -1
-assert sol.pivotIndexClean(nums=[1, 1]) == -1
-assert sol.pivotIndexClean(nums=[-1, -1, 0, 1, 1, -1]) == 0
-assert sol.pivotIndexClean(nums=[-1, -1, 0, 1, 1, 0]) == 5
+assert sol.pivotIndex(nums=[1, 7, 3, 6, 5, 6]) == 3
+assert sol.pivotIndex(nums=[1, 2, 3]) == -1
+assert sol.pivotIndex(nums=[1, 2, 1]) == 1
+assert sol.pivotIndex(nums=[2, 1, -1]) == 0
+assert sol.pivotIndex(nums=[1]) == 0
+assert sol.pivotIndex(nums=[1, 1]) == -1
+assert sol.pivotIndex(nums=[-1, -1, 0, 1, 1, -1]) == 0
+assert sol.pivotIndex(nums=[-1, -1, 0, 1, 1, 0]) == 5

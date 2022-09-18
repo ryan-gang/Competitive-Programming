5e7382a06ce4354f25cd7523424691c18033c7ea
Ryan Gang <ryan-gg@outlook.com>
2022-09-18 15:04:00 +0530

    1480. Running_Sum_of_1d_Array. Revision 1.

--- Leetcode/Running_Sum_of_1d_Array.py
+++ Leetcode/Running_Sum_of_1d_Array.py
@@ -1,9 +1,9 @@
 from typing import List
 
 
-# Runtime: 43 ms, faster than 82.81% of Python3 online submissions for Running Sum of 1d Array.
-# Memory Usage: 14.2 MB, less than 27.03% of Python3 online submissions for Running Sum of 1d Array.
 class Solution:
+    # Runtime: 43 ms, faster than 82.81% of Python3 online submissions.
+    # Memory Usage: 14.2 MB, less than 27.03% of Python3 online submissions.
     def runningSum(self, nums: List[int]) -> List[int]:
         out = []
         runningSum = 0
@@ -13,11 +13,9 @@ class Solution:
 
         return out
 
-
-# Runtime: 35 ms, faster than 97.54% of Python3 online submissions for Running Sum of 1d Array.
-# Memory Usage: 14 MB, less than 73.38% of Python3 online submissions for Running Sum of 1d Array.
-class SolutionOptimal:
-    def runningSum(self, nums: List[int]) -> List[int]:
+    # Runtime: 35 ms, faster than 97.54% of Python3 online submissions.
+    # Memory Usage: 14 MB, less than 73.38% of Python3 online submissions.
+    def runningSumInPlace(self, nums: List[int]) -> List[int]:
         for i in range(1, len(nums)):
             nums[i] += nums[i - 1]
 

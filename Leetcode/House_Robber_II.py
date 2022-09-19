e57aadbefcdf4183c4319423514537733a778b8f
Ryan Gang <ryan-gg@outlook.com>
2022-09-19 22:22:07 +0530

    213. House_Robber_II.

--- Leetcode/House_Robber_II.py
+++ Leetcode/House_Robber_II.py
@@ -2,12 +2,13 @@ from typing import List
 
 
 class Solution:
-    # 50/75 TC passed. Wrong answer.
-    # Breaks down for [1,2,1,1]
-    # The idea was, as we can rob atmost one out of the first and the last house,
-    # so we find the max, and store it in the first cell, and remove the last cell.
-    # But the problem with that logic is we are altering the structure of the nums,
-    # which will inevitably break the solution.
+    """50/75 TC passed. Wrong answer.
+    Breaks down for [1,2,1,1]
+    The idea was, as we can rob atmost one out of the first and the last house,
+    so we find the max, and store it in the first cell, and remove the last cell.
+    But the problem with that logic is we are altering the structure of the nums,
+    which will inevitably break the solution."""
+
     def robWrong(self, nums: List[int]) -> int:
         nums[0] = max(nums[0], nums[-1])
         if len(nums) == 1:
@@ -20,11 +21,11 @@ class Solution:
             prevprev, prev = prev, curr
         return prev
 
-    # Now, what we have here is circular pattern.
-    # Imagine, that we have 10 houses: a0, a1, a2, a3, ... a9: Then we have two possible options:
-    # Rob house a0, then we can not rob a1 or a9 and we have a2, a3, ..., a8 range to rob
-    # Do not rob house a0, then we have a1, a2, ... a9 range to rob.
-    # Then we just choose maximum of these two options and we are done!
+    """Now, what we have here is circular pattern.
+    Imagine, that we have 10 houses: a0, a1, a2, a3, ... a9: Then we have two possible options:
+    Rob house a0, then we can not rob a1 or a9 and we have a2, a3, ..., a8 range to rob
+    Do not rob house a0, then we have a1, a2, ... a9 range to rob.
+    Then we just choose maximum of these two options and we are done!"""
     # Runtime: 61 ms, faster than 22.76% of Python3 online submissions.
     # Memory Usage: 13.9 MB, less than 23.90% of Python3 online submissions.
     # T : O(N), S : O(1)
@@ -38,6 +39,15 @@ class Solution:
                 prevprev, prev = prev, curr
             return prev
 
+        """We could have done :
+        max(rob_helper(nums[1:]), rob_helper(nums[:-1]))
+        But in that case, if len(nums) == 1, we will need to have a separate case to return nums[0].
+        Because both times helper is called with a []. So to handle that edge case
+        gracefully, we are doing nums[0] + rob_helper(nums[2:-1]) instead of rob_helper(nums[:-1])
+        So this also acceptable :
+        if len(nums) == 1: return nums[0]
+        return max(rob_helper(nums[:-1]), rob_helper(nums[1:]))
+        """
         return max(nums[0] + rob_helper(nums[2:-1]), rob_helper(nums[1:]))
 
 

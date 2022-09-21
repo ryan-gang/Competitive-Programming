a6d1d92219377d17ddeceac3e575191f4d55b5ca
Ryan Gang <ryan-gg@outlook.com>
2022-09-21 21:23:54 +0530

    206. Reverse Linked List. Revision 1.

--- Leetcode/Reverse_Linked_List.py
+++ Leetcode/Reverse_Linked_List.py
@@ -1,8 +1,14 @@
 from typing import Optional
-from StarterCode.Linked_List_Utils import ListNode
+from StarterCode.Linked_List_Utils import ListNode, arrayToListNode, prettyPrintLinkedList
 
 
 class Solution:
+    """
+    If len = 0, initial value of prev (None) is returned. Expected.
+    If len = 1, loop runs for 1 iteration. curr.next is set to None.
+    And next iteration loop exits. As expected.
+    Much cleaner code."""
+
     # Runtime: 82 ms, faster than 6.32% of Python3 online submissions.
     # Memory Usage: 15.5 MB, less than 56.30% of Python3 online submissions.
     def reverseListClean(self, head: Optional[ListNode]) -> Optional[ListNode]:
@@ -15,8 +21,39 @@ class Solution:
             prev = curr
         return prev
 
+    """
+    Explicitly handling len = 0 and len = 1 cases.
+    Can be handled more gracefully by the above method."""
+    # Runtime: 90 ms, faster than 5.46% of Python3 online submissions.
+    # Memory Usage: 15.4 MB, less than 55.32% of Python3 online submissions.
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, node = None, head
+        if not node or not node.next:
+            return head
+        while node:
+            next = node.next
+            node.next = prev
+            prev, node = node, next
+
+        return prev
+
+    def reverseList2(self, head: Optional[ListNode]) -> Optional[ListNode]:
         cur, prev = head, None
         while cur:
             cur.next, prev, cur = prev, cur, cur.next
+            # Easier to understand :
+            # cur.next = prev
+            # prev, cur = cur, cur.next
         return prev
+
+
+sol = Solution()
+array = []
+array = [1]
+array = [1, 2]
+array = [1, 2, 3]
+array = [1, 2, 3, 4]
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+head = arrayToListNode(array)
+prettyPrintLinkedList(head)
+prettyPrintLinkedList(sol.reverseList(head))

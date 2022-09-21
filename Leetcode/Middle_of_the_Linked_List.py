80392437b18eff3a84b09738a1bcd95a2ae04c0a
Ryan Gang <ryan-gg@outlook.com>
2022-09-21 20:55:01 +0530

    876. Middle of the Linked List. Revision 1.

--- Leetcode/Middle_of_the_Linked_List.py
+++ Leetcode/Middle_of_the_Linked_List.py
@@ -10,8 +10,8 @@ class ListNode:
 
 
 class Solution:
-    # Runtime: 38 ms, faster than 78.73% of Python3 online submissions.
-    # Memory Usage: 14 MB, less than 11.16% of Python3 online submissions.
+    # Runtime: 24 ms, faster than 99.35% of Python3 online submissions.
+    # Memory Usage: 13.9 MB, less than 55.55% of Python3 online submissions.
     # T : O(N), S : O(1)
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast = slow = head
@@ -24,6 +24,12 @@ class Solution:
 
         return slow
 
+    """
+    Each time, slow go 1 steps while fast go 2 steps.
+    When fast arrives at the end, slow will arrive right in the middle. No need to think about
+    fast always being valid, let it be None. Slow is anyway behind,
+    it will come to the appropriate position."""
+
     def middleNodeOptimsedCode(self, head):
         slow = fast = head
         while fast and fast.next:
@@ -34,8 +40,8 @@ class Solution:
 
 sol = Solution()
 array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-# array = [1]
+array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+array = [1]
 head = arrayToListNode(array)
 prettyPrintLinkedList(head)
 prettyPrintLinkedList(sol.middleNode(head))

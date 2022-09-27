5098bfc558f478fd2860486afc3308500a7618f5
Ryan Gang <ryan-gg@outlook.com>
2022-09-27 19:09:58 +0530

    733. Flood Fill. Revision 1.

--- Leetcode/Flood_Fill.py
+++ Leetcode/Flood_Fill.py
@@ -12,13 +12,19 @@ class Solution:
         self.n = len(image[0])
         self.prev_color = self.image[sr][sc]
         if self.prev_color == color:
+            # Our recursion loop check code will fail if this is true. Endless recursion hell.
             return self.image
         self.fill(sr, sc, color)
         return self.image
 
     def fill(self, i, j, color):
+        # Here we have a check based on the array value, which helps to break recursion loops.
+        # A -> B -> A -> B ...
+        # If this check on prev_color was not present we would've had to pass a set of all the
+        # computed on indices to the fill method and then check on that.
         if self.image[i][j] == self.prev_color:
             self.image[i][j] = color
+            # Check boundary conditions before making the next call.
             if i < (self.m - 1):
                 self.fill(i + 1, j, color)
             if i > 0:

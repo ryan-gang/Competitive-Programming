bb11c88a66aeb8c1c0002ce08f2648411054ec35
Ryan Gang <ryan-gg@outlook.com>
2022-09-27 19:43:59 +0530

    200. Number of Islands. Revision 1.

--- Leetcode/Number_of_Islands.py
+++ Leetcode/Number_of_Islands.py
@@ -28,6 +28,41 @@ class SolutionDFS:
             self.dfs(grid, ii, jj)
 
 
+class SolutionDFS2:
+    # Runtime: 750 ms, faster than 21.00% of Python3 online submissions.
+    # Memory Usage: 16.4 MB, less than 81.29% of Python3 online submissions.
+    """To keep track of the visited islands, and not recount them, we rewrite them from "1" to "#".
+    If we encounter a "1", we dfs over all of its neighbours, and change them to "#",
+    it is one island only. And repeat this for all the "1"s.
+    We iterate over self.grid, and also change self.grid itself."""
+
+    def numIslands(self, grid: List[List[str]]) -> int:
+        self.grid = grid
+        self.m, self.n = len(grid), len(grid[0])
+        islands = 0
+
+        for i_idx, row in enumerate(self.grid):
+            for j_idx, cell in enumerate(row):
+                if cell == "1":
+                    self.dfs(i_idx, j_idx)
+                    islands += 1
+
+        return islands
+
+    def dfs(self, i, j):
+        if i < 0 or i >= self.m or j < 0 or j >= self.n or self.grid[i][j] != "1":
+            return
+        self.grid[i][j] = "#"
+        if i > 0:
+            self.dfs(i - 1, j)
+        if i < self.m - 1:
+            self.dfs(i + 1, j)
+        if j > 0:
+            self.dfs(i, j - 1)
+        if j < self.n - 1:
+            self.dfs(i, j + 1)
+
+
 class SolutionBFS:
     # Runtime: 678 ms, faster than 13.76% of Python3 online submissions.
     # Memory Usage: 16.2 MB, less than 91.69% of Python3 online submissions.

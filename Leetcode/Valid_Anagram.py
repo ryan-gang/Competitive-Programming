2d521bc53300fe19161b620277dffe1555e5a1b9
Ryan Gang <ryan-gg@outlook.com>
2022-10-15 19:08:31 +0530

    242. Valid Anagram. Revision 2.

--- Leetcode/Valid_Anagram.py
+++ Leetcode/Valid_Anagram.py
@@ -4,27 +4,24 @@ from collections import Counter
 class Solution:
     # Runtime: 64 ms, faster than 65.44%.
     # Memory Usage: 14.8 MB, less than 24.97%.
-    # T = O(NLogN)
-    # S = O(1)
+    # T : O(NLogN), S : O(1)
     def isAnagram1(self, s: str, t: str) -> bool:
         s = "".join(sorted(s))
         t = "".join(sorted(t))
         i = j = 0
         if len(s) != len(t):
             return False
-        else:
-            while i < len(s):
-                if s[i] == t[j]:
-                    i += 1
-                    j += 1
-                else:
-                    return False
+        while i < len(s):
+            if s[i] == t[j]:
+                i += 1
+                j += 1
+            else:
+                return False
         return True
 
     # Runtime: 78 ms, faster than 42.50%.
     # Memory Usage: 14.4 MB, less than 66.46%.
-    # T = O(N)
-    # S = O(N) ?
+    # T : O(N), S : O(1)
     def isAnagram2(self, s: str, t: str) -> bool:
         S = Counter(s)
         T = Counter(t)
@@ -43,8 +40,7 @@ class Solution:
 
     # Runtime: 55 ms, faster than 82.33%.
     # Memory Usage: 14.5 MB, less than 34.67%.
-    # T = O(N)
-    # S = O(1)
+    # T : O(N), S : O(1)
     def isAnagram(self, s: str, t: str) -> bool:
         array = [0 for i in range(26)]
         for char in s:
@@ -59,5 +55,6 @@ class Solution:
         return True
 
 
-# sol = Solution()
-# print(sol.isAnagram(s="rat", t="car"))
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.isAnagram(s="rat", t="car"))

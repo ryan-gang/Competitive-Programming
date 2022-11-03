8dcb3e0c9de079e6aa3ce00ff92abc62d2a4266b
Ryan Gang <ryan-gg@outlook.com>
2022-11-03 22:05:50 +0530

    141. Linked List Cycle. Revision 1.

--- Leetcode/Linked_List_Cycle.py
+++ Leetcode/Linked_List_Cycle.py
@@ -2,15 +2,15 @@ from typing import Optional
 from StarterCode.Linked_List_Utils import ListNode
 
 
-"""
-Hare and tortoise algo.
-fast node moves two nodes at a time, slow node moves one node at a time.
-If there is a loop present, at some point, they are bound to meet.
-When that happens, return True.
-Or if linked list ends, return False."""
-
-
 class Solution:
+    """
+    Hare and tortoise algo.
+    fast node moves two nodes at a time, slow node moves one node at a time.
+    If there is a loop present, at some point, they are bound to meet.
+    When that happens, return True.
+    Or if linked list ends, return False.
+    """
+
     # Runtime: 123 ms, faster than 15.55% of Python3 online submissions.
     # Memory Usage: 17.6 MB, less than 30.66% of Python3 online submissions.
     def hasCycle(self, head: Optional[ListNode]) -> bool:

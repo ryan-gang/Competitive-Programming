7867a595a052c7b476c8fe8bd66794f72498884f
Ryan Gang <ryan-gg@outlook.com>
2022-11-11 22:18:47 +0530

    200. Number of Islands. DSU implementation.

--- Leetcode/Number_of_Islands.py
+++ Leetcode/Number_of_Islands.py
@@ -1,6 +1,8 @@
 from typing import List
+from StarterCode.Union_Find import Union
 
 
+# Ref : discuss/56340/Python-Simple-DFS-Solution
 class SolutionDFS:
     # Runtime: 478 ms, faster than 53.52% of Python3 online submissions.
     # Memory Usage: 16.6 MB, less than 50.32% of Python3 online submissions.
@@ -18,7 +20,7 @@ class SolutionDFS:
         return islands
 
     def dfs(self, grid, i, j):
-        # Check for grid value, is there because dfs will call itself for all the neighbours.
+        # Check for grid value, because dfs will call itself for all the neighbours.
         # In those cases need to weed out spurious calls.
         if i < 0 or j < 0 or i >= self.m or j >= self.n or grid[i][j] != "1":
             return
@@ -29,13 +31,16 @@ class SolutionDFS:
 
 
 class SolutionDFS2:
-    # Runtime: 750 ms, faster than 21.00% of Python3 online submissions.
-    # Memory Usage: 16.4 MB, less than 81.29% of Python3 online submissions.
-    """To keep track of the visited islands, and not recount them, we rewrite them from "1" to "#".
+    """
+    To keep track of the visited islands, and not recount them,
+    we rewrite them from "1" to "#".
     If we encounter a "1", we dfs over all of its neighbours, and change them to "#",
     it is one island only. And repeat this for all the "1"s.
-    We iterate over self.grid, and also change self.grid itself."""
+    We iterate over self.grid, and also change self.grid itself.
+    """
 
+    # Runtime: 750 ms, faster than 21.00% of Python3 online submissions.
+    # Memory Usage: 16.4 MB, less than 81.29% of Python3 online submissions.
     def numIslands(self, grid: List[List[str]]) -> int:
         self.grid = grid
         self.m, self.n = len(grid), len(grid[0])
@@ -89,16 +94,53 @@ class SolutionBFS:
                 coordinates.append(grid, ii, jj)
 
 
-# Ref : https://leetcode.com/problems/number-of-islands/discuss/56340/Python-Simple-DFS-Solution
+# Runtime: 1325 ms, faster than 5.03% of Python3 online submissions.
+# Memory Usage: 18.4 MB, less than 39.00% of Python3 online submissions.
+class Solution:
+    """
+    Initially take a pass over the grid, add all islands as their own "sets"
+    in the Union Find data structure. In the second pass over grid, all nodes
+    and their neighbouring nodes will be "union"-ed, everytime we union we
+    reduce the count of islands by 1.
+    Finally we can return count of islands.
+    """
 
-sol = SolutionDFS()
-print(
-    sol.numIslandsdfs(
-        grid=[
-            ["1", "1", "1", "0", "0"],
-            ["1", "1", "0", "1", "0"],
-            ["0", "1", "0", "0", "0"],
-            ["1", "0", "0", "1", "1"],
-        ]
+    def numIslands(self, grid: List[List[str]]) -> int:
+        m, n = len(grid), len(grid[0])
+        uf = Union(m, n)
+        islands = 0
+
+        for r, row in enumerate(grid):
+            for c, cell in enumerate(row):
+                if cell == "1":
+                    node = r * n + c
+                    uf.new(node)
+                    islands += 1
+
+        for r, row in enumerate(grid):
+            for c, cell in enumerate(row):
+                if cell == "1":
+                    node1 = r * n + c
+                    for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                        rr, cc = r + dr, c + dc
+                        if rr >= 0 and rr < m and cc >= 0 and cc < n and grid[rr][cc] == "1":
+                            node2 = rr * n + cc
+                            res = uf.union(node1, node2)
+                            if res:
+                                islands -= 1
+
+        return islands
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(
+        sol.numIslands(
+            grid=[
+                ["1", "1", "1", "0", "0"],
+                ["1", "1", "0", "1", "0"],
+                ["0", "1", "0", "0", "0"],
+                ["1", "0", "0", "1", "1"],
+            ]
+        )
     )
-)

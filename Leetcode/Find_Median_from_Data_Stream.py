8347bf44bb88534102e6c40319bc3898bef4104f
Ryan Gang <ryan-gg@outlook.com>
2022-11-12 23:45:05 +0530

    295. Find Median from Data Stream.

--- Leetcode/Find_Median_from_Data_Stream.py
+++ Leetcode/Find_Median_from_Data_Stream.py
@@ -20,24 +20,23 @@ class MedianFinder:
         return (self.large[0] - self.small[0]) / 2.0
 
 
-class MedianFinderWrong:
-    """
-    Because of less feedback from small, both heaps go out of sorted order very fast.
-    Need to pop from small and push to large also sometimes.
-    """
-
+# Runtime: 539 ms, faster than 96.47% of Python3 online submissions.
+# Memory Usage: 36.1 MB, less than 57.44% of Python3 online submissions.
+class MedianFinder2:
     def __init__(self):
         self.small = []  # max_heap
         self.large = []  # min_heap
 
     def addNum(self, num):
-        heappush(self.large, num)
-        if len(self.large) - len(self.small) > 1:
-            heappush(self.small, -heappop(self.large))
+        smallest = heappushpop(self.large, num)
+        heappush(self.small, -smallest)
+        if len(self.small) - len(self.large) > 1:
+            largest = -heappop(self.small)
+            heappush(self.large, largest)
 
     def findMedian(self):
-        if len(self.large) > len(self.small):
-            return float(self.large[0])
+        if len(self.small) > len(self.large):
+            return -float(self.small[0])
         return (self.large[0] - self.small[0]) / 2.0
 
 

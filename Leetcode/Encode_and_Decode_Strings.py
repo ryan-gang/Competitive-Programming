6cee569c0d15aec7296e7741827f64a3ed491079
Ryan Gang <ryan-gg@outlook.com>
2022-11-22 21:04:26 +0530

    271. Encode and Decode Strings. Revision 1.

--- Leetcode/Encode_and_Decode_Strings.py
+++ Leetcode/Encode_and_Decode_Strings.py
@@ -1,8 +1,16 @@
 from typing import List
 
 
-# 611 ms 6.14MB 32.80% beat %.
+# 611 ms
+# 6.14MB 32.80%.
 class Solution:
+    """
+    Question is simple, but still I feel bad. I chose the delimiter intelligently.
+    If there was a word equal to my delimiter, my code would have broken.
+    So, other approach.
+    Using the word length as the delimiter. Will make the code a bit more complicated.
+    """
+
     DELIMITER = "↔↕"
     """
     @param: strs: a list of strings
@@ -24,12 +32,6 @@ class Solution:
         rev_strs = str[::-1]
         strings = rev_strs.split(Solution.DELIMITER)
         return strings
-
-
-# Question is simple, but still I feel bad. I chose the delimiter intelligently.
-# If there was a word equal to my delimiter, my code would have broken.
-# So, other approach.
-# Using the word length as the delimiter. Will make the code a bit more complicated.
 
 
 class Solution2:
@@ -65,7 +67,44 @@ class Solution2:
             # length digit(s)
             length = int(str[i:j])
             # append the word
-            res.append(str[j + 1: j + 1 + length])
+            res.append(str[j + 1 : j + 1 + length])
             # move pointer to end of the current word
             i = j + 1 + length
         return res
+
+
+class Solution3:
+    """
+    encode([ABC, DEFG, HIJ]) = ABC3|DEFG4|HIJ3|
+    string + len(string) + |
+    """
+
+    SPECIAL_CHAR = "|"
+
+    def encode(self, strs: List[str]) -> str:
+        encoded = []
+        for string in strs:
+            length = len(string)
+            encoded.append(f"{string}{length}{Solution3.SPECIAL_CHAR}")
+        enc_strs = "".join(encoded)
+        return enc_strs
+
+    def decode(self, string: str) -> List[str]:
+        strings = []
+        idx = 0
+        while idx < len(string):
+            while idx < len(string) and string[idx] != Solution3.SPECIAL_CHAR:
+                idx += 1
+            inferred_length = int(string[idx - 1])
+            strings.append(string[idx - inferred_length - 1 : idx - 1])
+            idx += 1
+        return strings
+
+
+if __name__ == "__main__":
+    sol = Solution3()
+    strings = ["ABC", "DEFGHIJ", "KLMNOP"]
+    enc = sol.encode(strings)
+    dec = sol.decode(enc)
+    print(f"Actual strings : {strings}\nEncoded strings : {enc}")
+    assert strings == dec

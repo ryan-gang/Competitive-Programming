5b6758e3f4a716efc0097e372a67d616d28f4bf3
Ryan Gang <ryan-gg@outlook.com>
2023-01-22 22:59:51 +0530

    131. Palindrome Partitioning.

---
+++ Leetcode/Palindrome_Partitioning.py
@@ -0,0 +1,47 @@
+from typing import List, Set
+
+
+class Solution:
+    # Runtime: 650 ms, faster than 90.48%.
+    # Memory Usage: 30.2 MB, less than 69.80%.
+    # T : O(N^2 + 2^N), S : O(N)
+    # N^2 for creating a list of all palindromes,
+    # 2^N for perusing all substrings, checking for palindromes.
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        palindromes: Set[str] = set()
+
+        for idx in range(n):
+            palindromes.add(s[idx])
+            lo, hi = idx - 1, idx + 1
+            while lo >= 0 and hi < n and s[lo] == s[hi]:
+                palindromes.add(s[lo : hi + 1])
+                lo -= 1
+                hi += 1
+
+            lo, hi = idx, idx + 1
+            while lo >= 0 and hi < n and s[lo] == s[hi]:
+                palindromes.add(s[lo : hi + 1])
+                lo -= 1
+                hi += 1
+
+        all_seqs: List[List[str]] = []
+
+        def dfs(seq: List[str], idx: int):
+            if idx == n:
+                all_seqs.append(seq[:])
+                return
+            for i in range(idx, n):
+                if s[idx : i + 1] in palindromes:
+                    seq.append(s[idx : i + 1])
+                    dfs(seq, i + 1)
+                    seq.pop()
+
+        dfs([], 0)
+        return all_seqs
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.partition(s="aab") == [["a", "a", "b"], ["aa", "b"]]
+    assert sol.partition(s="a") == [["a"]]

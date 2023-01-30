7c6b0edaefea619e11301f324e178afb5d0901d7
Ryan Gang <ryan-gg@outlook.com>
2023-01-31 00:47:28 +0530

    1137. N-th Tribonacci Number. Revision 1.

--- Leetcode/N-th_Tribonacci_Number.py
+++ Leetcode/N-th_Tribonacci_Number.py
@@ -1,4 +1,4 @@
-from functools import lru_cache
+from functools import cache
 
 
 class Solution:
@@ -7,8 +7,8 @@ class Solution:
     # T : O(3 ^ N), S : O(N) (Without LRU Cache)
     # T : O(N), S : O(N) (With LRU Cache)
     def trib_helper(self, n: int) -> int:
-        @lru_cache(maxsize=None)
-        def trib(n):
+        @cache
+        def trib(n: int) -> int:
             if n <= 2:
                 return [0, 1, 1][n]
             else:
@@ -38,14 +38,14 @@ class Solution:
     # Runtime: 49 ms, faster than 47.36% of Python3 online submissions.
     # Memory Usage: 13.9 MB, less than 12.83% of Python3 online submissions.
     # T : O(N), S : O(1)
-    def trib2(self, n: int) -> int:
+    def tribonacci(self, n: int) -> int:
         a, b, c = 0, 1, 1
         for _ in range(n):
             a, b, c = b, c, a + b + c
         return a
 
     # T : O(N), S : O(1)
-    def tribonacci(self, n):
+    def tribonacci1(self, n: int) -> int:
         dp = [0, 1, 1]
         for i in range(3, n + 1):
             dp[i % 3] = sum(dp)

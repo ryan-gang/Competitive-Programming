73183c9c9172bf8844665e43585660e8ae27d37f
Ryan Gang <ryan-gg@outlook.com>
2023-02-15 15:27:09 +0530

    474. Ones and Zeroes.

--- Leetcode/Ones and Zeroes.py
+++ Leetcode/Ones and Zeroes.py
@@ -1,65 +1,40 @@
-from typing import List
-
-
-# strs = ["10", "0001", "111001", "1", "0"]
-# m = 5
-# n = 3
-# strs, m, n = ["10", "0", "1"], 1, 1
-strs, m, n = ["001", "110", "0000", "0000"], 9, 2
-strs = sorted(strs, key=lambda item: len(item))
-out = 0
-
-
-def countBits(binary):
-    ones, zeros = 0, 0
-    for digit in binary:
-        if digit == "1":
-            ones += 1
-        else:
-            zeros += 1
-    return zeros, ones
-
-
-M, N = 0, 0
-for string in strs:
-    z, o = countBits(string)
-    M, N = M + z, N + o
-    if M > m or N > n:
-        M, N = M - z, N - z
-    else:
-        out += 1
-        print(string)
+from functools import cache
 
 
 class Solution:
-    # 24/71 TC passed.
-    def findMaxFormWrongBrute(self, strs: List[str], m: int, n: int) -> int:
-        out = set()
-        for _ in strs:
-            z, o = self.countBits(_)
-            if z <= m and o <= n:
-                out.append(_)
-        return len(out)
+    # Runtime: 2576 ms, faster than 83.41%.
+    # Memory Usage: 303.7 MB, less than 5.61%.
+    # T : O(N), S : O(N)
+    def findMaxForm(self, strs: list[str], m: int, n: int) -> int:
+        """
+        At every index we check how the inclusion and exclusion of that
+        string affects the final output.
+        Once, we include that string, and recurse to the next index.
+        And again we exclude it and recurse to the next index.
+        Then we can choose the one which gives the max answer.
+        """
+        N = len(strs)
 
-    def countBits(self, binary):
-        ones, zeros = 0, 0
-        for digit in binary:
-            if digit == "1":
-                ones += 1
-            else:
-                zeros += 1
-        return zeros, ones
+        @cache
+        def count(idx: int, zeroes: int, ones: int) -> int:
+            if zeroes < 0 or ones < 0:
+                return -1  # invalid subset, return -1.
+            if idx == N:
+                return 0  # Valid subset, but length = 0.
 
-    # 57/71 TC passed.
-    def findMaxFormGreedy(self, strs: List[str], m: int, n: int) -> int:
-        out = 0
-        strs = sorted(strs, key=lambda item: len(item))
-        M, N = 0, 0
-        for string in strs:
-            z, o = self.countBits(string)
-            M, N = M + z, N + o
-            if M > m or N > n:
-                M, N = M - z, N - z
-            else:
-                out += 1
-        return out
+            z, o = strs[idx].count("0"), strs[idx].count("1")
+
+            include_idx = 1 + count(idx + 1, zeroes - z, ones - o)
+            exclude_idx = count(idx + 1, zeroes, ones)
+
+            return max(include_idx, exclude_idx)
+
+        return max(count(0, m, n), 0)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.findMaxForm(strs=["10", "0001", "111001", "1", "0"], m=5, n=3) == 4
+    assert sol.findMaxForm(strs=["10", "0", "1"], m=1, n=1) == 2
+    assert sol.findMaxForm(strs=["10", "0001", "111001", "1", "0"], m=4, n=3) == 3
+    assert sol.findMaxForm(strs=["0", "0", "1", "1"], m=2, n=2) == 4

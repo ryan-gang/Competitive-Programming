17b97a3fe546584a555d7b3095d3b71edf83956f
Ryan Gang <ryan-gg@outlook.com>
2023-02-21 00:38:01 +0530

    35. Search Insert Position. Revision 1.

--- Leetcode/Search_Insert_Position.py
+++ Leetcode/Search_Insert_Position.py
@@ -1,3 +1,4 @@
+from bisect import bisect_left
 from typing import List
 
 
@@ -9,17 +10,20 @@ class Solution:
         lo, hi = 0, len(nums) - 1
         while lo <= hi:
             mid = (lo + hi) // 2
-            if nums[mid] == target:
-                return mid
+            if nums[mid] < target:
+                lo = mid + 1
             elif nums[mid] > target:
                 hi = mid - 1
             else:
-                lo = mid + 1
-
+                return mid
         return lo
 
+    def searchInsert1(self, nums: List[int], target: int) -> int:
+        return bisect_left(nums, target)
 
-sol = Solution()
-assert sol.searchInsert(nums=[1, 3, 5, 6], target=5) == 2
-assert sol.searchInsert(nums=[1, 3, 5, 6], target=2) == 1
-assert sol.searchInsert(nums=[1, 3, 5, 6], target=7) == 4
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.searchInsert(nums=[1, 3, 5, 6], target=5) == 2
+    assert sol.searchInsert(nums=[1, 3, 5, 6], target=2) == 1
+    assert sol.searchInsert(nums=[1, 3, 5, 6], target=7) == 4

7dc080b59bd19df2251b0fca81e6ecdf575581c1
Ryan Gang <ryan-gg@outlook.com>
2023-03-08 20:56:29 +0530

    704. Binary Search. Revision 1.

--- Leetcode/Binary_Search.py
+++ Leetcode/Binary_Search.py
@@ -1,11 +1,8 @@
-from typing import List
-
-
 class Solution:
     # Runtime: 251 ms, faster than 94.37% of Python3 online submissions.
     # Memory Usage: 15.4 MB, less than 97.68% of Python3 online submissions.
-    # T : O(lgN), S : O(1)
-    def search(self, nums: List[int], target: int) -> int:
+    # T : O(logN), S : O(1)
+    def search(self, nums: list[int], target: int) -> int:
         lo, hi = 0, len(nums) - 1
         while lo <= hi:
             mid = (lo + hi) // 2
@@ -18,6 +15,29 @@ class Solution:
 
         return -1
 
+    # Runtime: 240 ms, faster than 82.49%.
+    # Memory Usage: 15.5 MB, less than 59.87%.
+    # T : O(LogN), S : O(1)
+    # Using the template @ StarterCode.binary_search
+    def search1(self, nums: list[int], target: int) -> int:
+        def condition(array: list[int], value: int):
+            return array[value] >= target
 
-sol = Solution()
-assert sol.search(nums=[-1, 0, 3, 5, 9, 12], target=9) == 4
+        def binary_search(array: list[int]) -> int:
+            left, right = 0, len(array)
+            while left < right:
+                mid = left + (right - left) // 2
+                if condition(array, mid):
+                    right = mid
+                else:
+                    left = mid + 1
+            return left
+
+        idx = binary_search(nums)
+        return idx if idx >= 0 and idx < len(nums) and nums[idx] == target else -1
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.search1(nums=[-1, 0, 3, 5, 9, 12], target=9) == 4
+    assert sol.search1(nums=[-1, 0, 3, 5, 9, 12], target=13) == -1

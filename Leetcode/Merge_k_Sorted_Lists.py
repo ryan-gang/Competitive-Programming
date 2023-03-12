4d7aebad3ad186019a57e61f91576b532b54c07c
Ryan Gang <ryan-gg@outlook.com>
2023-03-13 01:07:30 +0530

    23. Merge k Sorted Lists. Revision 1.

--- Leetcode/Merge_k_Sorted_Lists.py
+++ Leetcode/Merge_k_Sorted_Lists.py
@@ -1,29 +1,31 @@
-from typing import List, Optional
+from typing import Optional
 from StarterCode.Linked_List_Utils import ListNode, arrayToListNode, prettyPrintLinkedList
 from heapq import heappush, heappop
 
 
 class Solution:
     """
-    We will create a new head, to which we will keep on adding nodes from the other linked lists.
-    At every iteration we need to find the lowest value from the "k" heads.
-    And then add this head to our new list, and set this head to its head.next.
+    We create a new head, onto which we keep on adding nodes from the other linked lists.
+    At every iteration we find the lowest value from the "k" heads, add this head to our
+    new list, and update this head to its head.next.
+    To find out the min value from the "k" heads, we use a min heap.
     """
 
-    # Runtime: 213 ms, faster than 61.03% of Python3 online submissions.
-    # Memory Usage: 18.2 MB, less than 38.58% of Python3 online submissions.
+    # Runtime: 107 ms, faster than 67.65%.
+    # Memory Usage: 17.8 MB, less than 46.85%.
     # Insertions into the heap with at most K items will be O(log K), this is repeated
     # for NxK times. No extra space for new linked list, at most O(K) space for heap.
     # T : O(N x K x log K), S : O(K)
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+    def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
         new_head = curr = ListNode(0)
-        heap = []
+        heap: list[tuple[int, int]] = []
         for idx, node in enumerate(lists):
             if node:
                 heappush(heap, (node.val, idx))
 
         while heap:
-            lowest_val, idx = heappop(heap)
+            _, idx = heappop(heap)  # We fetch the smallest value.
+            # And add this node from its list to our output.
             head = lists[idx]
             headnext = head.next
             curr.next = head
@@ -39,9 +41,7 @@ class Solution:
 if __name__ == "__main__":
     sol = Solution()
     arrays = [[1, 4, 5, 5, 5, 5, 10], [1, 3, 4, 5, 6, 8, 11], [2, 6, 7, 8, 8, 10, 11]]
-    # arrays = [[]]
-    # arrays = []
-    lists = []
+    lists: list[Optional["ListNode"]] = []
     for array in arrays:
         head = arrayToListNode(array)
         lists.append(head)

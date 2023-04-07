5c2a75e3f7b13130f71708afafdc9f50c7028d9e
Ryan Gang <ryan-gg@outlook.com>
2023-04-07 23:59:51 +0530

    172. Factorial Trailing Zeroes. Revision 1.

--- Leetcode/Factorial_Trailing_Zeroes.py
+++ Leetcode/Factorial_Trailing_Zeroes.py
@@ -1,6 +1,7 @@
 class Solution:
     # Runtime: 41 ms, faster than 86.86%.
     # Memory Usage: 13.8 MB, less than 61.81%.
+    # T : O(LogN), S : O(1)
     def trailingZeroes(self, n: int) -> int:
         """
         Find out the total number of 2's in the factorial, and the total number of 5's.
@@ -19,10 +20,21 @@ class Solution:
 
         return min(twos, fives)
 
+    def trailingZeroes1(self, n: int) -> int:
+        def get_factors(val: int) -> int:
+            factors, exp = 0, val
+            while exp <= n:
+                factors += n // exp
+                exp *= val
+            return factors
+
+        return min(get_factors(2), get_factors(5))
+
 
 if __name__ == "__main__":
     sol = Solution()
     assert sol.trailingZeroes(n=999) == 246
+    assert sol.trailingZeroes(n=200) == 49
     assert sol.trailingZeroes(n=0) == 0
     assert sol.trailingZeroes(n=5) == 1
     assert sol.trailingZeroes(n=3) == 0

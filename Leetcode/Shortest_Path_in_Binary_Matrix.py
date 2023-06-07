1fe03ff796238a9c322a573e909e19373cd3b15d
Ryan Gang <ryan-gg@outlook.com>
2023-06-07 14:40:37 +0530

    1091. Shortest Path in Binary Matrix. Revision 1.

--- Leetcode/Shortest_Path_in_Binary_Matrix.py
+++ Leetcode/Shortest_Path_in_Binary_Matrix.py
@@ -1,91 +1,78 @@
-from typing import List, Set
+from collections import deque
+from typing import Deque
 
 
 class Solution:
-    # 56 / 89 TC passed. TLE.
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        self.grid = grid
-        self.m, self.n = len(grid), len(grid[0])
-        self.start, self.end = (0, 0), (self.m - 1, self.n - 1)
-        self.shortest_path_length = float("inf")
-        v = set()
-        if self.grid[0][0] == 0:
-            v.add((0, 0))
-            self.traverse(0, 0, 1, v)
-        if self.shortest_path_length == float("inf"):
+    """
+    Both solutions utilise BFS to find the shortest distance, in the first one
+    we do level wise traversal of the queue, and keep track of levels. In the
+    second one we store the level, inside the queue with every element.
+    """
+    # T : O(N ^ 2), S : O(N)
+    def shortestPathBinaryMatrix(self, grid: list[list[int]]) -> int:
+        n, length = len(grid), 1
+        if grid[0][0] or grid[n - 1][n - 1]:
             return -1
-        return self.shortest_path_length
+        queue: Deque[tuple[int, int]] = deque()
+        queue.append((0, 0))
+        grid[0][0] = 1
 
-    def traverse(self, i, j, path_length, visited_indices: Set):
-        if path_length > self.shortest_path_length:
-            return
-        if (i, j) == self.end:
-            self.shortest_path_length = min(self.shortest_path_length, path_length)
-        for row in range(i - 1, i + 2):
-            for col in range(j - 1, j + 2):
-                if (
-                    row >= 0
-                    and row < self.m
-                    and col >= 0
-                    and col < self.n
-                    and (row, col) != (i, j)
-                    and (row, col) not in visited_indices
-                    and self.grid[row][col] == 0
-                ):
-                    visited_indices.add((row, col))
-                    self.traverse(row, col, path_length + 1, visited_indices)
-                    visited_indices.remove((row, col))
+        diff = [-1, 0, 1]
 
+        while queue:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                if r == n - 1 and c == n - 1:
+                    return length
+                for dr in diff:
+                    for dc in diff:
+                        rr, cc = r + dr, c + dc
+                        if (
+                            0 <= rr < n
+                            and 0 <= cc < n
+                            and grid[rr][cc] == 0
+                            and (not (dr == dc == 0))
+                        ):
+                            queue.append((rr, cc))
+                            grid[rr][cc] = 1
 
-class SolutionOnlyRightAndDownTraverse:
-    # 62/89 TC passed. Wrong answer.
-    # Because this code doesn't traverse left or upwards.
-    # So TC like this : [[0,1,1,0,0,0],[0,1,0,1,1,0],[0,1,1,0,1,0],[0,0,0,1,1,0],[1,1,1,1,1,0],[1,1,1,1,1,0]]
-    # Will break the code.
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        self.grid = grid
-        self.m, self.n = len(grid), len(grid[0])
-        self.start, self.end = (0, 0), (self.m - 1, self.n - 1)
-        self.shortest_path_length = float("inf")
-        if self.grid[0][0] == 0:
-            self.traverse(0, 0, 1)
-        if self.shortest_path_length == float("inf"):
+            length += 1
+
+        return -1
+
+    def shortestPathBinaryMatrix1(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] or grid[n - 1][n - 1]:
+            # If start or end cells are already 1 we can't step on them.
             return -1
-        return self.shortest_path_length
+        q: list[tuple[int, int, int]] = [(0, 0, 1)]  # row, col, distance
+        grid[0][0] = 1
+        diff = [-1, 0, 1]
+        for i, j, d in q:
+            if i == n - 1 and j == n - 1:
+                return d
+            for xx in diff:
+                for yy in diff:
+                    x, y = i + xx, j + yy
+                    if (
+                        (xx != 0 or yy != 0)
+                        and 0 <= x < n
+                        and 0 <= y < n
+                        and grid[i + xx][j + yy] == 0
+                    ):
+                        grid[x][y] = 1
+                        q.append((x, y, d + 1))
+        return -1
 
-    def traverse(self, i, j, path_length):
-        if path_length > self.shortest_path_length:
-            return
-        if (i, j) == self.end:
-            self.shortest_path_length = min(self.shortest_path_length, path_length)
-        for row in range(i, i + 2):
-            for col in range(j, j + 2):
-                if (
-                    row >= 0
-                    and row < self.m
-                    and col >= 0
-                    and col < self.n
-                    and (row, col) != (i, j)
-                    and self.grid[row][col] == 0
-                ):
-                    self.traverse(row, col, path_length + 1)
 
-
-sol = Solution()
-assert (sol.shortestPathBinaryMatrix(grid=[[0, 0, 0], [1, 1, 0], [1, 1, 0]])) == 4
-assert sol.shortestPathBinaryMatrix(grid=[[1, 0, 0], [1, 1, 0], [1, 1, 0]])
-assert sol.shortestPathBinaryMatrix(grid=[[0, 1], [1, 0]]) == 2
-print(
-    sol.shortestPathBinaryMatrix(
-        [
-            [0, 0, 1, 0, 0, 0, 1, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1, 0, 0, 0],
-            [1, 1, 1, 0, 0, 0, 0, 1],
-            [1, 0, 0, 1, 0, 1, 1, 0],
-            [0, 0, 0, 0, 0, 1, 1, 0],
-            [1, 0, 0, 0, 0, 1, 1, 0],
-        ]
+if __name__ == "__main__":
+    sol = Solution()
+    assert (sol.shortestPathBinaryMatrix(grid=[[0, 0, 0], [1, 1, 0], [1, 1, 0]])) == 4
+    assert sol.shortestPathBinaryMatrix(grid=[[1, 0, 0], [1, 1, 0], [1, 1, 0]]) == -1
+    assert sol.shortestPathBinaryMatrix(grid=[[0, 1], [1, 0]]) == 2
+    assert (
+        sol.shortestPathBinaryMatrix(
+            [[0, 0, 0, 0, 1], [1, 0, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 1, 1], [0, 0, 0, 1, 0]]
+        )
+        == -1
     )
-)

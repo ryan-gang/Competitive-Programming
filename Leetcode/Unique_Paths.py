7fefecf94006853f3d3f01525596208019fb1d43
Ryan Gang <ryan-gg@outlook.com>
2023-09-03 21:24:46 +0530

    62. Unique Paths. Revision 1.

--- Leetcode/Unique_Paths.py
+++ Leetcode/Unique_Paths.py
@@ -2,9 +2,9 @@ from functools import lru_cache
 
 
 class Solution:
-    # Runtime: 32 ms, faster than 93.66% of Python3 online submissions.
-    # Memory Usage: 14.4 MB, less than 73.91% of Python3 online submissions.
-    # T : O(N), S : O(N) (Call stack)
+    # Runtime: 32 ms, faster than 93.66%.
+    # Memory Usage: 14.4 MB, less than 73.91%.
+    # T : O(M*N), S : O(M*N) (Call stack)
     @lru_cache(maxsize=None)
     def uniquePathsRecurse(self, m: int, n: int) -> int:
         if (m, n) == (1, 1):
@@ -14,10 +14,21 @@ class Solution:
 
         return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n - 1)
 
-    # Runtime: 68 ms, faster than 10.30% of Python3 online submissions.
-    # Memory Usage: 13.8 MB, less than 73.91% of Python3 online submissions.
-    def uniquePathsIterative(self, m: int, n: int) -> int:
-        prev = [0] * (n + 1)
+    # T : O(M*N), S : O(M*N)
+    def uniquePathsIterative1(self, m: int, n: int) -> int:
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[1][1] = 1
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if not (i == 1 and j == 1):
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m][n]
+
+    # Runtime: 68 ms, faster than 10.30%.
+    # Memory Usage: 13.8 MB, less than 73.91%.
+    # T : O(M*N), S : O(N)
+    def uniquePathsIterative2(self, m: int, n: int) -> int:
+        prev = current = [0] * (n + 1)
         for i in range(1, m + 1):
             current = [0] * (n + 1)
             # Total Ways to come to current cell, is the
@@ -32,13 +43,14 @@ class Solution:
 
         return current[-1]
 
-    # Runtime: 53 ms, faster than 40.27% of Python3 online submissions.
-    # Memory Usage: 13.8 MB, less than 98.30% of Python3 online submissions.
-    # Unique ways to come to the entire 1st row and entire 1st col is "1". Always.
+    # Runtime: 53 ms, faster than 40.27%.
+    # Memory Usage: 13.8 MB, less than 98.30%.
+    # Unique ways to come to the entire 1st row and entire 1st col is always 1.
     # Now from 1, 1 : We can make turns. As we can only move down or right, ways to come to a cell,
     # is the ways to come to the cell on its left + ways to come to the cell on its top.
     def uniquePaths(self, m: int, n: int) -> int:
         dp = [[1 for _ in range(n)] for _ in range(m)]
+        # Or start with all 0s and put 1s in first row and col.
         for i in range(1, m):
             for j in range(1, n):
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
@@ -46,13 +58,11 @@ class Solution:
         return dp[-1][-1]
 
 
-# Ref : https://leetcode.com/problems/unique-paths/
-# discuss/267043/Python3-Solution-backtracking-with-memoization.
-sol = Solution()
-
-assert sol.uniquePaths(m=3, n=7) == 28
-assert sol.uniquePaths(m=3, n=2) == 3
-assert sol.uniquePaths(m=3, n=3) == 6
-assert sol.uniquePaths(m=3, n=4) == 10
-assert sol.uniquePaths(m=4, n=5) == 35
-print(sol.uniquePathsIterative(m=100, n=100))
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.uniquePaths(m=3, n=7) == 28
+    assert sol.uniquePaths(m=3, n=2) == 3
+    assert sol.uniquePaths(m=3, n=3) == 6
+    assert sol.uniquePaths(m=3, n=4) == 10
+    assert sol.uniquePaths(m=4, n=5) == 35
+    print(sol.uniquePathsIterative1(m=100, n=100))

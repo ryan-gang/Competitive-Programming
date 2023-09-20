a6420b90b71e8e419523ad44fa96d78dcf690761
Ryan Gang <ryan-gg@outlook.com>
2023-09-20 14:14:25 +0530

    15. 3Sum.

--- Leetcode/3Sum.py
+++ Leetcode/3Sum.py
@@ -1,39 +1,60 @@
-from typing import List
-
-
 class Solution:
-    # Runtime: 1287 ms, faster than 54.32% of Python3 online submissions.
-    # Memory Usage: 18.1 MB, less than 72.13% of Python3 online submissions.
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    # Runtime: 1287 ms, faster than 54.32%.
+    # Memory Usage: 18.1 MB, less than 72.13%.
+    # We fix one pointer, and then iterate the list using 2 pointers,
+    # to get progressively closer to our target.
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
-        out = []
+        out: list[list[int]] = []
 
         n = len(nums)
-        for lo in range(0, n - 2):
-            mid = lo + 1
-            hi = n - 1
-            target = 0 - nums[lo]
-            if lo > 0 and nums[lo] == nums[lo - 1]:
+        for left in range(0, n - 2):
+            mid = left + 1
+            right = n - 1
+            if left > 0 and nums[left] == nums[left - 1]:
+                # Ignoring duplicates
                 continue
-            while mid < hi:
-                added_value = nums[mid] + nums[hi]
-                if added_value == target:
-                    out.append([nums[lo], nums[mid], nums[hi]])
-                    while mid < hi and nums[mid] == nums[mid + 1]:
+            while mid < right:
+                curr_sum = nums[left] + nums[mid] + nums[right]
+                if curr_sum < 0:
+                    mid += 1
+                elif curr_sum > 0:
+                    right -= 1
+                else:  # curr_sum == 0
+                    out.append([nums[left], nums[mid], nums[right]])
+                    while mid < right and nums[mid] == nums[mid + 1]:
+                        # Ignoring duplicates
                         mid += 1
-                    while mid < hi and nums[hi] == nums[hi - 1]:
-                        hi -= 1
+                    while mid < right and nums[right] == nums[right - 1]:
+                        # Ignoring duplicates
+                        right -= 1
                     mid += 1
-                    hi -= 1
-                elif added_value > target:
-                    hi -= 1
-                else:
-                    mid += 1
+                    right -= 1
 
         return out
 
+    # Using a dict, we keep track of all the values in our list, and then
+    # get the sum of all combinations of 2 elements taken together, and
+    # see if its complement is there in the dict or not.
+    def threeSum1(self, nums: list[int]) -> list[list[int]]:
+        d = {val: idx for idx, val in enumerate(nums)}
 
-sol = Solution()
-assert sol.threeSum(nums=[-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
-assert sol.threeSum(nums=[0, 1, 1]) == []
-assert sol.threeSum(nums=[0, 0, 0]) == [[0, 0, 0]]
+        n = len(nums)
+        res: set[tuple[int, int, int]] = set()
+        for i in range(n):
+            for j in range(i + 1, n):
+                add = nums[i] + nums[j]
+                if -add in d:
+                    idx = d[-add]
+                    if i != idx and j != idx:
+                        values = sorted([nums[i], nums[j], -add])
+                        res.add(tuple(values))  # type: ignore
+
+        return list(res)  # type: ignore
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    assert sol.threeSum(nums=[-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
+    assert sol.threeSum(nums=[0, 1, 1]) == []
+    assert sol.threeSum(nums=[0, 0, 0]) == [[0, 0, 0]]
